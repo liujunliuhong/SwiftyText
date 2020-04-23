780513714e360f0555b8222ff12c2db57518ed2c
liujun <1035841713@qq.com>
2020-04-23 19:45:59 +0800

    first commit

---
+++ SwiftyText/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SwiftyText
+//
+//  Created by apple on 2020/4/23.
+//  Copyright © 2020 yinhe. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ SwiftyText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyText
+//
+//  Created by apple on 2020/4/23.
+//  Copyright © 2020 yinhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
