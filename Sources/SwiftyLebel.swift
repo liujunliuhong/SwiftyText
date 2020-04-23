955c6d01551aab741ac9d8af3b22fdce8b3c78b8
liujun <1035841713@qq.com>
2020-04-23 19:48:10 +0800

    创建SwiftyLabel

---
+++ Sources/SwiftyLebel.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLebel.swift
+//  SwiftyText
+//
+//  Created by apple on 2020/4/23.
+//  Copyright © 2020 yinhe. All rights reserved.
+//
+
+import UIKit
+
+class SwiftyLebel: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
